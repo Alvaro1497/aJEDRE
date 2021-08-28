605e7ad6c9360e27a4e0a945512ee757da63f004
Alvaro1497 <88108161+Alvaro1497@users.noreply.github.com>
2021-08-27 20:21:05 -0500

    Update Pieza.java

--- src/tablero/Pieza.java
+++ src/tablero/Pieza.java
@@ -2,19 +2,7 @@ package tablero;
 
 public class Pieza extends Tablero  {
 
-	public String nombre;
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	@Override
-	public String toString() {
-		return "\nPieza [nombre= " + nombre + "]";
-	}
-
+	private String nombrePieza;
+	private String colorPieza;
+	
 }
