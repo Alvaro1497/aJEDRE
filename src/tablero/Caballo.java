be1904814af7a18439fccac3b09adb0d4e86215b
Alvaro1497 <88108161+Alvaro1497@users.noreply.github.com>
2021-08-27 20:22:11 -0500

    Update Caballo.java

--- src/tablero/Caballo.java
+++ src/tablero/Caballo.java
@@ -1,24 +1,20 @@
 package tablero;
+public class Caballo extends Pieza {
 
-import java.util.Arrays;
-
-public class Caballo extends Tablero{
-
-	public  void caballoB() {
-		Pieza caballoB = new Pieza ();
-		caballoB.setNombre("CB1");
-		
-		Pieza caballoB2 = new Pieza ();
-		caballoB2.setNombre("CB2");
-		
-		tablero [0][1]="CB1";
-		tablero [0][6]="CB2";
-			
-		
-	
-	
-	
-
+	private String caballoBlanca;
+	private String caballoNegro;
+	public String getCaballoBlanca() {
+		return caballoBlanca;
+	}
+	public void setCaballoBlanca(String caballoBlanca) {
+		this.caballoBlanca = caballoBlanca;
+	}
+	public String getCaballoNegro() {
+		return caballoNegro;
+	}
+	public void setCaballoNegro(String caballoNegro) {
+		this.caballoNegro = caballoNegro;
 	}
 
+	
 }
