a7f4e6d0acf529d414b47fe48d2d8e8bf553f8c6
Alvaro1497 <88108161+Alvaro1497@users.noreply.github.com>
2021-08-27 20:17:46 -0500

    Update Tablero.java

--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -1,23 +1,50 @@
 package tablero;
+public abstract class   Tablero  {
 
-public class Tablero {
+	public   void main(String[] args) {
+		Peon peonBlanco = new Peon();
+		peonBlanco.setPeonBlanca("Peon Blanco");
+
+		Peon peonNegro = new Peon();
+		peonNegro.setPeonNegra("Peon Negro");
+
+		Torre torreBlanca = new Torre();
+		torreBlanca.setTorreBlanca("Torre Blanco");
+
+		Torre torreNegra = new Torre();
+		torreNegra.setTorreNegra("Torre negro");
+
+		Caballo caballoBlanco = new Caballo();
+		caballoBlanco.setCaballoBlanca("Caballo Blanco");
+
+		Caballo caballoNegro = new Caballo();
+		caballoNegro.setCaballoNegro("caballo negro");
+
+		Alfil alfilBlanco = new Alfil();
+		alfilBlanco.setAlfilBlanca("alfil blanco");
+		
+		Alfil alfilNegro = new Alfil();
+		alfilNegro.setAlfilNegra("alfil negro");		
+
+		Reina reinaBlanca = new Reina();
+		reinaBlanca.setReinaBlanca("reina blanca ");
+
+		Reina reinaNegra = new Reina();
+		reinaNegra.setReinaNegra("Reina negra");
+
+		ReyB reyBlanco = new ReyB();
+		reyBlanco.setReyBlanca("rey blanco");
+
+		ReyB reyNegro = new ReyB();
+		reyNegro.setReyNegra("REY NEGRO");
+		
+        Tablero  tablero[][]= new Tablero[8][8];
+		tablero [0][4] = reinaBlanca; 
+		tablero [7][4]= reinaNegra;
+		
+
+		
 	
-	 static String tablero[][] = new String[8][8];
-	
-	public static void main(String[] args) {
-		for (int i = 0; i <8; i++) {
-			for (int j = 0; j < 8; j++) {
-
-			}
-		}
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j <8; j++) {
-				System.out.print(tablero + " ");
-
-			}
-			System.out.println();
-		}
-
 	}
 
-}
+	}
