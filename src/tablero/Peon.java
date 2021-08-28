c235bb0a71e11b6d89c358f15780a5ea051a463f
Alvaro1497 <88108161+Alvaro1497@users.noreply.github.com>
2021-08-27 20:20:31 -0500

    Update Peon.java

--- src/tablero/Peon.java
+++ src/tablero/Peon.java
@@ -2,44 +2,23 @@ package tablero;
 
 public class Peon extends Tablero  {
 
-	public  void peonB() {
-			
-		Pieza peonB1 = new Pieza ();
-		peonB1.setNombre("PB1");
-		
-		Pieza peonB2 = new Pieza ();
-		peonB2.setNombre("PB2");
-		
-		Pieza peonB3 = new Pieza ();
-		peonB3.setNombre("PB3");
-		
-		Pieza peonB4 = new Pieza ();
-		peonB4.setNombre("PB4");
-		
-		Pieza peonB5 = new Pieza ();
-		peonB5.setNombre("PB5");
-		
-		Pieza peonB6 = new Pieza ();
-		peonB6.setNombre("PB6");
-		
-		Pieza peonB7 = new Pieza ();
-		peonB7.setNombre("PB7");
-		
-		Pieza peonB8 = new Pieza ();
-		peonB8.setNombre("PB8");
-		
-		tablero [1][0]="PB1";
-		tablero [1][1]="PB2";
-		tablero [1][2]="PB3";
-		tablero [1][3]="PB4";
-		tablero [1][4]="PB5";
-		tablero [1][5]="PB6";
-		tablero [1][6]="PB7";
-		tablero [1][7]="PB8";
-		
-		
-		
-		
-	}
+	ppublic class Peon extends Pieza {
 
+	private String peonBlanca;
+	private String peonNegra;
+	
+	public String getPeonBlanca() {
+		return peonBlanca;
+	}
+	public void setPeonBlanca(String peonBlanca) {
+		this.peonBlanca = peonBlanca;
+	}
+	
+	public String getPeonNegra() {
+		return peonNegra;
+	}
+	public void setPeonNegra(String peonNegra) {
+		this.peonNegra = peonNegra;
+	}
+}	
 }
